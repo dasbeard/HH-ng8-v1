1ab3e9d9c821a3f1207f110120db19ed67693bf8
Bryan Jones <thebeard@me.com>
2019-08-23 17:33:36 -0700

    At a stoping point until data arrives

--- src/app/Components/services/services.component.ts
+++ src/app/Components/services/services.component.ts
@@ -6,7 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./services.component.scss']
 })
 export class ServicesComponent implements OnInit {
-
+  checked1;
+  checked2:boolean = true;
+  checked3:boolean = false;
+  checked4:boolean = false;
+  checked5:boolean = true;
+  checked6:boolean = false;
+  checked7:boolean = true;
+  
+  
   constructor() { }
 
   ngOnInit() {
