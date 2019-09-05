73a98de32db644b6f250c678b78614b4cb97c623
Bryan Jones <thebeard@me.com>
2019-09-04 19:02:19 -0700

    Working on Register component

--- src/app/Components/register-account/register-account.component.ts
+++ src/app/Components/register-account/register-account.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Validators, FormGroup, FormBuilder } from "@angular/forms";
+import { Router } from "@angular/router";
+import { AuthService } from "../../Services/auth.service";
 
 @Component({
   selector: 'app-register-account',
@@ -9,10 +12,63 @@ export class RegisterAccountComponent implements OnInit {
 
   @Output() goToLogin = new EventEmitter<boolean>();
 
-  constructor() { }
+  hidePass: boolean = true;
+  hidePassConf: boolean = true;
+
+  registerForm: FormGroup;
+  isSubmitted = false;
+
+  constructor( private authService: AuthService, private router: Router, private formBuilder: FormBuilder ) { }
 
   ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      organization: ["",Validators.required],
+      email: ["",Validators.required],
+      password: ["",Validators.required],
+      confirmPassword: ["",Validators.required]
+    })
+
   }
+
+  get formControls() {
+    return this.registerForm.controls;
+  }
+
+  register(){
+    console.log(this.registerForm.value);
+    this.isSubmitted = true;
+    if (this.registerForm.invalid) {
+      this.registerForm.reset();
+      return;
+    }
+
+    let pass =this.registerForm.get('password').value;
+    let confPass =this.registerForm.get('confirmPassword').value;
+        
+    let passwordsMatch = this.verifyPasswords(pass, confPass) 
+     
+    if(passwordsMatch) {
+      this.authService.login(this.registerForm.value);
+      this.router.navigateByUrl("/OrgAdmin");
+    } else {
+      alert('Passwords dont match')
+    }
+
+    
+
+  }
+
+
+  verifyPasswords(password, confirmPassword) {
+    console.log('in pass check');
+    
+    if(password === confirmPassword){
+      return true
+    } else {
+      return false
+    }
+  }
+
 
   login() {
     this.goToLogin.emit(true);
