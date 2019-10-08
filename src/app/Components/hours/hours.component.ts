42f341a990d3885d5a394d673c9d2d00048a14f4
Bryan Jones <thebeard@me.com>
2019-10-07 18:23:30 -0700

    Hours component on OrgPage is done

--- src/app/Components/hours/hours.component.ts
+++ src/app/Components/hours/hours.component.ts
@@ -1,34 +1,5 @@
 import { Component, OnInit, Input } from "@angular/core";
 
-export interface Hours {
-  day: string;
-  open: string;
-  spacer: string;
-  close: string;
-}
-
-// const hoursOfOperation1: Hours[];
-//  = [
-//   // {day: 'Mon', open: '12:30am', spacer: '-', close: '11:15pm'},
-//   // {day: 'Tue', open: '1:30am', spacer: '-', close: '1:45pm'},
-//   // {day: 'Wen', open: '10:30am', spacer: '-', close: '12:00pm'},
-//   // {day: 'Thu', open: '9:00am', spacer: '-', close: '5:15pm'},
-//   // {day: 'Fri', open: '4:30am', spacer: '-', close: '6:30pm'},
-//   // {day: 'Sat', open: '3:45am', spacer: '-', close: '8:15pm'},
-//   // {day: 'Sun', open: '7:15am', spacer: '-', close: '12:45pm'}
-// ];
-
-// const hoursServingFood1: Hours[];
-//  = [
-//   { day: "Mon", open: "1:30am", spacer: "-", close: "1:15pm" },
-//   { day: "Tue", open: "12:30am", spacer: "-", close: "4:45pm" },
-//   { day: "Wen", open: "5:30am", spacer: "-", close: "11:00pm" },
-//   { day: "Thu", open: "7:00am", spacer: "-", close: "7:15pm" },
-//   { day: "Fri", open: "3:30am", spacer: "-", close: "9:30pm" },
-//   { day: "Sat", open: "4:45am", spacer: "-", close: "7:15pm" },
-//   { day: "Sun", open: "2:15am", spacer: "-", close: "2:45pm" }
-// ];
-
 @Component({
   selector: "app-hours",
   templateUrl: "./hours.component.html",
@@ -39,57 +10,36 @@ export class HoursComponent implements OnInit {
   @Input() hoursServing: object;
 
   hoursDisplayed: string;
+  hoursToDisplay: object;
 
-  displayedColumns: string[] = ["day", "open", "spacer", "close"];
-
-  HofOp: Hours[] = [];
-  //  = hoursOfOperation1;
-  serving: Hours[] = [];
-  //  = hoursServingFood1;
+  days: Array<string> = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday"
+  ];
 
   constructor() {
-    this.hoursDisplayed = "HofOp";
+    this.hoursDisplayed = "hoursOfOperation";
   }
-
+  
   ngOnInit() {
-    // console.log(this.hoursDisplayed);
-
-    this.addSpacer(this.hoursOfOp, "HofOp");
-    if (this.hoursServing) {
-      this.addSpacer(this.hoursServing, "serving");
-    }
-  }
-
-  addSpacer(data, arrayName) {
-    if (arrayName == "HofOp") {
-      Object.keys(data).forEach(day => {
-        data[day].day = day;
-        if (data[day].isClosed === true) {
-          data[day].spacer = "Closed";
-        } else {
-          data[day].spacer = "-";
-        }
-        this.HofOp.push(data[day]);
-      });
-    } else if (arrayName == "serving") {
-      Object.keys(data).forEach(day => {
-        data[day].day = day;
-        if (data[day].isClosed === true) {
-          data[day].spacer = "Closed";
-        } else {
-          data[day].spacer = "-";
-        }
-        this.serving.push(data[day]);
-      });
-    }
+    // console.log(this.hoursOfOp);
+    // console.log(this.hoursServing);
+    this.hoursToDisplay = this.hoursOfOp;
   }
 
   changeHoursView(input) {
-    if (input == "HofOp") {
-      this.hoursDisplayed = "HofOp";
+    if (input == "hoursOfOperation") {
+      this.hoursDisplayed = "hoursOfOperation";
+      this.hoursToDisplay = this.hoursOfOp;
     }
-    if (input == "serving") {
-      this.hoursDisplayed = "serving";
+    if (input == "hoursServing") {
+      this.hoursDisplayed = "hoursServing";
+      this.hoursToDisplay = this.hoursServing;
     }
   }
 }
