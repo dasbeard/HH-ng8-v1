f82259c870fd832008655ce5bfb68e2966f3f5a4
Bryan Jones <thebeard@me.com>
2019-10-09 11:00:29 -0700

    Switching to work on HomePage Map

--- src/app/Pages/home-page/home-page.component.ts
+++ src/app/Pages/home-page/home-page.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit } from "@angular/core";
 import { GeolocationService } from "src/app/Services/geolocation.service";
 import { OrganizationsService } from "src/app/Services/organizations.service";
+import { AgmInfoWindow } from "@agm/core";
 
 interface geoLocation {
   lat: number;
   lng: number;
   zoom: number;
-};
-
+}
 
 @Component({
   selector: "app-home-page",
@@ -15,20 +15,27 @@ interface geoLocation {
   styleUrls: ["./home-page.component.scss"]
 })
 export class HomePageComponent implements OnInit {
-  location: geoLocation ={
+  location: geoLocation = {
     lat: 34.05,
     lng: -118.25,
-    zoom: 9,
-  }
-  
-  currentDate;
+    zoom: 9
+  };
 
+  currentDate;
   allOrgs;
+
+  // !! NEED TO GET FIRST 3 ORGS
+
+  currentIW: AgmInfoWindow;
+  previousIW: AgmInfoWindow;
 
   constructor(
     private geoService: GeolocationService,
     private orgService: OrganizationsService
-  ) {}
+  ) {
+    this.currentIW = null;
+    this.previousIW = null;
+  }
 
   ngOnInit() {
     this.runGeoLocation();
@@ -59,15 +66,29 @@ export class HomePageComponent implements OnInit {
   getAllOrgs() {
     this.orgService.getAllOrgs().subscribe(data => {
       this.allOrgs = data;
+
+      console.log(this.allOrgs);
     });
+  }
+
+  mapClick() {
+    if (this.previousIW) {
+      this.previousIW.close();
+    }
+  }
+
+  markerClick(infoWindow) {
+    if (this.previousIW) {
+      this.currentIW = infoWindow;
+      this.previousIW.close();
+    }
+    this.previousIW = infoWindow;
   }
 
   getCurrentTime() {
     this.currentDate = new Date();
 
     // console.log(this.currentDate.get);
-    console.log('day', this.currentDate.getDay());
-    
+    console.log("day", this.currentDate.getDay());
   }
-
 }
